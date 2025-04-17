3a3ba24e4899bc66cf69fa2ae3da37267f5fe58b
EC2 Default User <ec2-user@ip-172-31-5-203.ap-northeast-1.compute.internal>
2025-04-17 03:56:37 +0000

    [Add]comment

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,2 +1,25 @@
 class Public::CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    post = Post.find(params[:post_id])
+    comment = current_user.comments.new(comment_params)
+    comment.post_id = post.id
+    comment.save
+    redirect_to post_path(post)
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    if comment.user == current_user
+      comment.destroy
+    end
+    redirect_to post_path(comment.post)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
