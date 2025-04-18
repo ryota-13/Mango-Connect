a4b993ec0e1ca02973cc1067ec642b78abb35c20
EC2 Default User <ec2-user@ip-172-31-5-203.ap-northeast-1.compute.internal>
2025-04-18 03:30:29 +0000

    [Add]admin-users admin-posts

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,25 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!
+  before_action :set_user, only: [:show, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @posts = @user.posts
+  end
+
+  def destroy
+    @user.destroy
+    flash[:notice] = "ユーザーを退会処理しました"
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
