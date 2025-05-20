521125ecd3de3c17354835603adf851fd3004c9b
EC2 Default User <ec2-user@ip-172-31-5-203.ap-northeast-1.compute.internal>
2025-05-20 06:05:28 +0000

    [Add]admin_searsh

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,2 +1,17 @@
 class Admin::SearchesController < ApplicationController
+  before_action :authenticate_admin!
+
+  def search
+    @model = params[:model] || 'user'
+    @content = params[:content]
+    @method = params[:method] || 'partial'
+
+    if @model == "user"
+      @records = User.search_for(@content, @method).page(params[:page]).per(10)
+    elsif @model == "post"
+      @records = Post.search_for(@content, @method).page(params[:page]).per(10)
+    else
+      @records = []
+    end
+  end
 end
